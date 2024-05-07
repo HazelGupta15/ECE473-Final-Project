0a21ec8d2eeb007e3d92d0a0642d8722e650de22
Kaustuv Mukherjee <158002498+Kaustuv-Mukherjee@users.noreply.github.com>
2024-05-07 16:23:21 -0400

    Update DAOToken.sol

--- DAOToken.sol
+++ DAOToken.sol
@@ -1,4 +1,7 @@
 // SPDX-License-Identifier: MIT
+// CAPTCHA ERC20 token to represent voting power in our DAO. Implemented lock and unlock functions for CCUS storage facilities 
+// to bring to market or lock available credits
+
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
